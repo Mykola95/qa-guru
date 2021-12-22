c966b79f14bb67a9da17c49776305cf51aad6deb
Mykola <mykola.danylo@izzi.software>
2021-12-22 17:08:31 +0200

    added examples on selenide

---
+++ src/test/java/alfabank/AlfabankTest.java
@@ -0,0 +1,19 @@
+package alfabank;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class AlfabankTest {
+    @Test
+    void testAlfa() {
+        open("https://alfabank.ru");
+        $(byText("Вклады")).click();
+        $("body").shouldHave(text("Накопительные продукты"));
+        $(byText("Депозиты")).click();
+    }
+}
