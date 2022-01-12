ceb215bc14698ec18f78887ed5604055e13d686f
Mykola <mykola.danylo@izzi.software>
2022-01-12 10:46:17 +0200

    added some changes

---
+++ src/test/java/com/demoqa/pageobjects/pages/RegistrationPage.java
@@ -0,0 +1,27 @@
+package com.demoqa.pageobjects.pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class RegistrationPage {
+    SelenideElement
+            firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName");
+
+    public void openPage() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+    }
+
+    public void typeFirstName(String value) {
+        firstNameInput.setValue(value);
+    }
+
+    public void typeLastName(String value) {
+        lastNameInput.setValue(value);
+    }
+
+}
--- src/test/java/com/demoqa/selenidestyle/checkhomework/StudentsRegistrationFormTests.java
+++ src/test/java/com/demoqa/selenidestyle/checkhomework/StudentsRegistrationFormTests.java
@@ -1,4 +1,4 @@
-package b.checkhomework;
+package com.demoqa.selenidestyle.checkhomework;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
